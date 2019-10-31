2edff285753effe3df459ed222071fbc1e4613d9
Ankit Mahato <ankmahato@gmail.com>
2019-10-31 14:38:25 +0530

    Fix regex replace all for bytes input

--- titus/lib/regex.py
+++ titus/lib/regex.py
@@ -529,7 +529,7 @@ class ReplaceAll(LibFcn):
         region = re.getRegion()
         beg = 0
         end = region.beg[0]
-        out = ""
+        out = b"" if isinstance(haystack, bytes) else ""
         if found:
             while found:
                 out = out + to(haystack[beg:end]) + replacement
